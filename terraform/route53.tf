c6a12e15b5be2708f5283baab187f25adb25b526
Philip Nwachukwu <philip.i.nwachukwu@gmail.com>
2023-03-27 10:57:55 +0000

    Update project

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,5 +1,5 @@
 # Route53 Hosted Zone
-resource "aws_route53_zone" "apache" {
+resource "aws_route53_zone" "eks" {
   name = "philipnwachukwu.ml"
 }
 
@@ -9,12 +9,21 @@ resource "aws_route53_record" "ns" {
   name            = "philipnwachukwu.ml"
   ttl             = 172800
   type            = "NS"
-  zone_id         = aws_route53_zone.apache.zone_id
+  zone_id         = aws_route53_zone.eks.zone_id
 
   records = [
-    aws_route53_zone.apache.name_servers[0],
-    aws_route53_zone.apache.name_servers[1],
-    aws_route53_zone.apache.name_servers[2],
-    aws_route53_zone.apache.name_servers[3],
+    aws_route53_zone.eks.name_servers[0],
+    aws_route53_zone.eks.name_servers[1],
+    aws_route53_zone.eks.name_servers[2],
+    aws_route53_zone.eks.name_servers[3],
   ]
+
+}
+
+resource "aws_route53_record" "fineract" {
+  zone_id = aws_route53_zone.eks.zone_id
+  name    = "sockshop.philipnwachukwu.ml/fineract"
+  type    = "CNAME"
+  ttl     = 3600
+  records = [module.eks.cluster_endpoint]
 }
