28c182be446449ecbde8e83f8b8bac4b493c3668
Philip Nwachukwu <philip.i.nwachukwu@gmail.com>
2023-03-29 17:19:13 +0000

    update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -132,20 +132,4 @@ resource "aws_eks_addon" "ebs-csi" {
   }
 }
 
-resource "null_resource" "merge_kubeconfig" {
-  triggers = {
-    always = timestamp()
-  }
 
-  depends_on = [module.eks]
-
-  provisioner "local-exec" {
-    interpreter = ["/bin/bash", "-c"]
-    command     = <<EOT
-      set -e
-      echo 'Applying Auth ConfigMap with kubectl...'
-      aws eks wait cluster-active --name '${local.cluster_name}'
-      aws eks update-kubeconfig --name '${local.cluster_name}' --alias '${local.cluster_name}-${var.region}' --region '${var.region}'
-    EOT
-  }
-}
