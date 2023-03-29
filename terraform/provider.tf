c31c07c6d1b78f282c98287dad86059eb90939db
Philip Nwachukwu <philip.i.nwachukwu@gmail.com>
2023-03-29 15:41:05 +0000

    update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,8 +1,9 @@
 terraform {
   required_providers {
+
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 4.47.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
@@ -13,6 +14,22 @@ terraform {
       source  = "hashicorp/helm"
       version = "~> 2.0"
     }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.4.3"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.2.0"
+    }
+
   }
 }
 
@@ -24,6 +41,8 @@ provider "kubernetes" {
   config_path = "~/.kube/config"
   #   config_context = "my-context"
 }
+
+
 
 
 
