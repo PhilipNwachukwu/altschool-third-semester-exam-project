24629c5ea3dcae4e40efc528f9623ef2b7453d3d
Philip Nwachukwu <philip.i.nwachukwu@gmail.com>
2023-03-29 20:25:30 +0000

    update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -54,6 +54,24 @@ provider "kubernetes" {
   }
 }
 
+provider "helm" {
+  kubernetes {
+    config_path            = "~/.kube/config"
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1"
+      command     = "aws"
+      args = [
+        "eks",
+        "get-token",
+        "--cluster-name",
+        module.eks.cluster_name
+      ]
+    }
+  }
+}
+
 
 
 
